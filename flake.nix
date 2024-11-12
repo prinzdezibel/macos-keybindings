f7fb6d4e6cf0b843e0279c6c7dbcf01d156f604b
Michael Jenny <michael.jenny@projekt-und-partner.de>
2024-11-12 15:59:19 +0100

    Comments.

--- flake.nix
+++ flake.nix
@@ -22,26 +22,7 @@
     { self, ... }@inputs:
     {
       nixosModules = rec {
-        #macos-keybindings = (import ./modules);
-        home-manager = inputs.home-manager.nixosModules.home-manager;
         macos-keybindings = (import ./modules {inherit (inputs) home-manager plasma-manager;});
-        
-        #macos-keybindings = (import ./modules);
-        #macos-keybindings = (import ./modules {inherit inputs;}) // {home-manager = inputs.home-manager; plasma-manager = inputs.plasma-manager;};
-        # macos-keybindings = inputs.nixpkgs.lib.mkMerge [
-        #   (import ./modules)
-
-        #   {plasma-manager = inputs.plasma-manager;}
-        # ];
-
-        # macos-keybindings = rec {
-        #   inherit (import ./modules);
-        #   #home-manager = inputs.home-manager;
-        #   #inherit (inputs) plasma-manager;
-        # };
-
-        
-
         default = macos-keybindings;
       };
     };
